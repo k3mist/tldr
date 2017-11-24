afe27c04c684a43b9f28e71f9a012492fcda243c
Daniel Robbins <daniel@danieljrobbins.com>
2017-11-24 11:41:00 -0500

    update screenshot

--- main.go
+++ main.go
@@ -72,10 +72,10 @@ func tldr() error {
 
 func banner() {
 	fmt.Print("" +
-		color.ColorNormal(color.Blue) + `   ___________   _____  _____  ` + "\n" +
-		color.ColorNormal(color.Cyan) + `  /__   __/  /  /  _  \/  _  \ ` + "\n" +
-		color.ColorNormal(color.Cyan) + `    /  / /  /  /  //  /  //  / ` + "\n" +
-		color.ColorNormal(color.Blue) + `   /  / /  /__/  //  /  / \  \ ` + color.ColorBold(color.White) + "tldr.sh\n" +
-		color.ColorNormal(color.Cyan) + `  /__/ /_____/______/__/   \_/ ` + color.ColorBold(color.DarkGray) + "bitbucket.org/djr2/tldr\n\n" + color.Reset,
+		color.Color(color.Blue) + `   ___________   _____  _____  ` + "\n" +
+		color.Color(color.Cyan) + `  /__   __/  /  /  _  \/  _  \ ` + "\n" +
+		color.Color(color.Cyan) + `    /  / /  /  /  //  /  //  / ` + "\n" +
+		color.Color(color.Blue) + `   /  / /  /__/  //  /  / \  \ ` + color.ColorBold(color.White) + "tldr.sh\n" +
+		color.Color(color.Cyan) + `  /__/ /_____/______/__/   \_/ ` + color.ColorBold(color.DarkGray) + "bitbucket.org/djr2/tldr\n\n" + color.Reset,
 	)
 }
