2083588dfc8446677b03ff442a8fb2ec23adacc4
Daniel Robbins <daniel@danieljrobbins.com>
2025-07-18 09:48:04 -0400

    minor cleanup in messages

--- messages.go
+++ messages.go
@@ -15,7 +15,8 @@ func banner() {
 		color.Color(cfg.BannerColor2) + `  /__   __/  /  /  _  \/  _  \ ` + "\n" +
 		color.Color(cfg.BannerColor2) + `    /  / /  /  /  //  /  //  / ` + "\n" +
 		color.Color(cfg.BannerColor1) + `   /  / /  /__/  //  /  / \  \ ` + "\n" +
-		color.Color(cfg.BannerColor2) + `  /__/ /_____/______/__/   \_/ ` + color.ColorBold(cfg.TLDRColor) + "https://tldr.sh\n\n" + color.Reset,
+		color.Color(cfg.BannerColor2) + `  /__/ /_____/______/__/   \_/ ` +
+		color.ColorBold(cfg.TLDRColor) + "https://tldr.sh\n\n" + color.Reset,
 	)
 }
 
@@ -23,26 +24,31 @@ func version() {
 	banner()
 	cfg := config.Config
 	fmt.Print(
-		color.Color(cfg.BannerColor1) + `  version: ` + color.ColorBold(cfg.TLDRColor) + "2.1.0" + color.Reset + "\n",
+		color.Color(cfg.BannerColor1) + `  version: ` +
+			color.ColorBold(cfg.TLDRColor) + "2.1.0" +
+			color.Reset + "\n",
 	)
 }
 
 func noTldr(cmd string) {
 	cfg := config.Config
-	fmt.Println(color.Color(cfg.DescriptionColor) + "> Unable to find a tldr for " + color.Color(cfg.HeaderColor) + cmd + color.Reset)
+	fmt.Println(color.Color(cfg.DescriptionColor) + "> Unable to find a tldr for " +
+		color.Color(cfg.HeaderColor) + cmd + color.Reset)
 }
 
 func noLookup(cmd string, lang string, plat platform.Platform) {
 	cfg := config.Config
 	if cfg.LookupWarnings || flagWarn {
-		fmt.Print(color.Color(cfg.DescriptionColor) + "> No tldr for ")
-		fmt.Print(color.Color(cfg.HeaderDecorColor) + "[")
-		fmt.Print(color.ColorBold(cfg.HeaderColor) + cmd)
-		fmt.Print(color.Color(cfg.HeaderDecorColor) + "] language[")
-		fmt.Print(color.ColorBold(cfg.HeaderColor) + lang)
-		fmt.Print(color.Color(cfg.HeaderDecorColor) + "] platform[")
-		fmt.Print(color.ColorBold(cfg.HeaderColor) + plat.String())
-		fmt.Print(color.Color(cfg.HeaderDecorColor) + "]")
-		fmt.Println(color.Reset)
+		fmt.Println(
+			color.Color(cfg.DescriptionColor) + "> No tldr for " +
+				color.Color(cfg.HeaderDecorColor) + "[" +
+				color.ColorBold(cfg.HeaderColor) + cmd +
+				color.Color(cfg.HeaderDecorColor) + "] language[" +
+				color.ColorBold(cfg.HeaderColor) + lang +
+				color.Color(cfg.HeaderDecorColor) + "] platform[" +
+				color.ColorBold(cfg.HeaderColor) + plat.String() +
+				color.Color(cfg.HeaderDecorColor) + "]" +
+				color.Reset,
+		)
 	}
 }
